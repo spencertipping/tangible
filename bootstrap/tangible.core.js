7d14da48ddab795f15a411534380004ce7eecb1d
Spencer Tipping <spencer@spencertipping.com>
2012-02-19 15:29:21 -0700

    Implemented a rewriter that fails miserably.

--- bootstrap/tangible.core.js
+++ bootstrap/tangible.core.js
@@ -1,2 +1,2 @@
-caterwaul.module( 'tangible.core' ,function(c) {$=c,$.require=require,$.tangible=function(image) {;
+caterwaul.module( 'tangible.core' ,function(c) {$=c.merge(c, {require:require,process:process} ) ,$.tangible=function(image) {;
 return( ( (tangible= (caterwaul.serialization() ) .decode(image) ) ) , (tangible.init() ) ) } } ) ;
